0b4b45e56dd1ecb42505ddb1595de5771f4dcaba
MarinaKondratyk <Marina Kondratyk>
2019-01-20 22:20:34 +0400

    Были созданы часы

---
+++ js/script.js
@@ -0,0 +1,31 @@
+window.onload = function () {
+    function clock() {
+        var date = new Date(),
+            day = date.getDate(),
+            month = date.getMonth(),
+            // monthArr = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"],
+            year = date.getFullYear(),
+            hour = date.getHours(),
+            min = date.getMinutes(),
+            sec = date.getSeconds();
+
+        if (day < 10) day = "0" + day;
+        if (month < 10) month = "0" + ++month;
+        if (hour < 10) hour = "0" + hour;
+        if (min < 10) min = "0" + min;
+        if (sec < 10) sec = "0" + sec;
+
+        document.getElementById("date").innerHTML = day + "/" + month;
+        document.getElementById("year").innerHTML = year;
+        document.getElementById("time2").innerHTML = hour + " " + ":" + " " + min + " " + ":" + " " + sec;
+
+    }
+
+    var timer;
+
+    function clockStart() {
+        timer = setInterval(clock, 1000);
+        clock();
+    }
+clockStart();
+}
