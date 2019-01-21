77b7c855c6928c1184ccc4f1254f3a72531e65fa
MarinaKondratyk <Marina Kondratyk>
2019-01-21 23:08:17 +0400

    Были подключены часы

--- js/script.js
+++ js/script.js
@@ -3,19 +3,18 @@ window.onload = function () {
         var date = new Date(),
             day = date.getDate(),
             month = date.getMonth(),
-            // monthArr = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"],
+            monthArr = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"],
             year = date.getFullYear(),
             hour = date.getHours(),
             min = date.getMinutes(),
             sec = date.getSeconds();
 
         if (day < 10) day = "0" + day;
-        if (month < 10) month = "0" + ++month;
         if (hour < 10) hour = "0" + hour;
         if (min < 10) min = "0" + min;
         if (sec < 10) sec = "0" + sec;
 
-        document.getElementById("date").innerHTML = day + "/" + month;
+        document.getElementById("date").innerHTML = day + "/" + monthArr[month];
         document.getElementById("year").innerHTML = year;
         document.getElementById("time2").innerHTML = hour + " " + ":" + " " + min + " " + ":" + " " + sec;
 
